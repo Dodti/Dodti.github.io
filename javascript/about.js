084b6296078fea02b5fdce72a886c8b689d539ab
Nashdodti <41152814+Nashdodti@users.noreply.github.com>
2020-09-10 22:39:53 +0530

    c

--- javascript/about.js
+++ javascript/about.js
@@ -1,7 +1,1 @@
-$('.kevin-mortensen-2').delay(2000).animate({'opacity':'1'},2500); 
-
-// SCROLL FADE IN //
- AOS.init({
-  duration: 1200,
-})
-// SCROLL FADE IN //
+$(".kevin-mortensen-2").delay(2e3).animate({opacity:"1"},2500),AOS.init({duration:1200});
